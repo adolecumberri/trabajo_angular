3cd2e55b72a48f27ae08877b4285f25aee7fec29
jesus <antoniofarroyo86@gmail.com>
2019-12-27 13:12:47 +0100

    update the database fields and therefore the controllers

--- back/controllers/moviesController.js
+++ back/controllers/moviesController.js
@@ -11,8 +11,8 @@ const msg = "ERROR";
 controller.getMovies = (req, res) => {
   const { userId } = req.params;
 
-  connection.query(`SELECT title,director, main_actor, release_date FROM movies 
-  LEFT JOIN user_movie ON movies.movie_id = user_movie.movie_id 
+  connection.query(`SELECT title,director, main_actor, release_date, category, status
+  FROM movies LEFT JOIN user_movie ON movies.movie_id = user_movie.movie_id 
   WHERE user_movie.user_id = '${userId}'`,
     (err, results) => {
       if (err) res.send(msg)
@@ -24,7 +24,7 @@ controller.getMovies = (req, res) => {
 controller.getMovie = (req, res) => {
   const movieId = req.params.movieId;
 
-  connection.query(`SELECT title, director, main_actor, release_date FROM movies
+  connection.query(`SELECT title, director, main_actor, release_date, category, status FROM movies
     WHERE movie_id = '${movieId}'`,
     (err, results) => {
       if (err) res.send(msg)
